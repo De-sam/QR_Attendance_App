b4ca8677d6c0ac3f382b306f3cf99966eb893744
De-sam <samsoncoded@gmail.com>
2024-04-18 23:36:37 +0100

    added signup and login authentication

--- myapp/auth.py
+++ myapp/auth.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
-from myapp import render_template,request
+from myapp import render_template,request,flash,redirect,url_for
+from . import db
+from .models import User
 
 auth = Blueprint("auth", __name__)
 
@@ -11,10 +13,27 @@ def login():
 
 @auth.route("/signup", methods=['GET','POST'])
 def signup():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    confirm_password = request.form.get('confirm_password')
-    print(email)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+
+        email_exists = User.query.filter_by(email=email).first()
+        if email_exists:
+            flash('email is already in use.', category='error')
+        elif password != confirm_password:
+            flash('Your password and your confirm password are not the same!', category='error')
+        elif len(password) < 6:
+            flash('your password is too short, it must have 6 chracters or more!')
+        elif len(email) < 4:
+            flash('invalid email address! Please use a valid email.', category='error')
+        else:
+            new_user = User(email = email, password = forgot_password )
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User created! Signup was sucessful', category='success')
+            return redirect(url_for('views.home'))
+            
     return render_template('login_signup.html')
 
  
