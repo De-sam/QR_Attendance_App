47611ec4e9c5935dcf2f915ea4233c541ae071a2
De-sam <samsoncoded@gmail.com>
2024-04-28 04:45:22 +0100

    updating the dashboard

--- myapp/views.py
+++ myapp/views.py
@@ -12,3 +12,18 @@ def home():
 @login_required
 def dashboard():
     return render_template('dashboard_base.html', name=current_user.username)
+
+@views.route("/create_org")
+@login_required
+def create_org():
+    return render_template('create_org.html', name=current_user.username)
+
+@views.route("/manage_org")
+@login_required
+def manage_org():
+    return render_template('manage_org.html', name=current_user.username)
+
+@views.route("/join_org")
+@login_required
+def join_org():
+    return render_template('join_org.html', name=current_user.username)
