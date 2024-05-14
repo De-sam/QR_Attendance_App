eb834e7eba10dc27b5d126cc36df02aeb0b1a2d5
De-sam <samsoncoded@gmail.com>
2024-05-14 22:11:54 +0100

    Fix app structure and gunicorn config

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from myapp import create_app
 import os
 
-
+app = create_app()
 
 
 if __name__=='__main__':
-    app = create_app()
+  
     debug_mode = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't']
     app.run(host='0.0.0.0', port=10000, debug=debug_mode)
 
