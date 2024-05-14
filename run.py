b0ea35d85bfea93266f6c74bae94fa3d6aaeae2f
De-sam <samsoncoded@gmail.com>
2024-05-14 21:31:09 +0100

    added: scan functionality,attendance log functionality and set deadline for lateness functionality

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ import os
 if __name__=='__main__':
     app = create_app()
     debug_mode = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't']
-    app.run(host='0.0.0.0', debug=debug_mode)
+    app.run(host='0.0.0.0', port=10000, debug=debug_mode)
 
