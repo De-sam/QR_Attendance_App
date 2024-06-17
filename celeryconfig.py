d0b9e03e73a358e4fd3fda471868aa6272fc1ae2
De-sam <samsoncoded@gmail.com>
2024-06-17 19:02:23 +0100

    update: Added auto clockout

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,17 +1,24 @@
+from celery import Celery
 from celery.schedules import crontab
+import os
 
-CELERY_BEAT_SCHEDULE = {
+app = Celery('myapp',
+             broker=os.getenv('BROKER_URL', 'redis://red-cpo2mtdds78s73b95tpg:6379/0'),
+             backend=os.getenv('RESULT_BACKEND', 'redis://red-cpo2mtdds78s73b95tpg:6379/0'))
+
+app.conf.update(
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json'],
+    timezone='UTC',
+    enable_utc=True,
+    broker_connection_retry_on_startup=True,
+)
+
+app.conf.beat_schedule = {
     'auto-clock-out-every-2-minutes': {
         'task': 'myapp.tasks.auto_clock_out',
         'schedule': crontab(minute='*/2'),  # Every 2 minutes
     },
 }
 
-broker_url = 'redis://red-cpo2mtdds78s73b95tpg:6379'
-result_backend = 'redis://red-cpo2mtdds78s73b95tpg:6379'
-task_serializer = 'json'
-result_serializer = 'json'
-accept_content = ['json']
-timezone = 'UTC'
-enable_utc = True
-broker_connection_retry_on_startup = True
